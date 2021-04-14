953e1587f43cce0c1aabf7a207d8dd8413f08334
songliang <isongliang@qq.com>
2021-04-14 15:39:06 +0800

    ch1-第一版，带有 bug，浏览器无法正常渲染

--- src/main/java/com/tomcat/learn/ch1/Response.java
+++ src/main/java/com/tomcat/learn/ch1/Response.java
@@ -30,13 +30,21 @@ public class Response {
             File file = new File(HttpServer.WEB_ROOT, request.getUri());
             if (file.exists()) {
                 fis = new FileInputStream(file);
+                //源代码中没有下面这段话，导致请求可能会出现错误。现在添加。
+                String msg = "HTTP/1.1 200 File 随便填的 Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n";
+                output.write(msg.getBytes());
                 int ch = fis.read(bytes, 0, BUFFER_SIZE);
-                while (ch != -1) {
+                System.out.println("response  **");
+                while (ch!=-1) {
                     output.write(bytes, 0, ch);
                     ch = fis.read(bytes, 0, BUFFER_SIZE);
                 }
-            } else {
-                String errorMessage = "HTTP/1.1 404 FileNoe";
+           } else {
+                String errorMessage = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type: text/html\r\n"
+                        + "Content-Length: 23\r\n" + "\r\n" + "<h1>File Not Found</h1>";
+                output.write(errorMessage.getBytes());
             }
         } catch (Exception e) {
             System.out.println(e.toString());
